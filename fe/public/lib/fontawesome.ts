3b05ccbc777cde1afd58d5be998109fa0ab912a1
Cipppp <pirvuciprian37@gmail.com>
2024-05-15 21:34:38 +0300

    Updated IUstudio website part 3

--- fe/public/lib/fontawesome.ts
+++
@@ -1,9 +0,0 @@
-// lib/fontawesome.ts
-import { library, config } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons'; // Import all free solid icons
-import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the necessary styles
-
-config.autoAddCss = false; // Avoid double loading of CSS
-
-// Add the icons you intend to use
-library.add(fas);
