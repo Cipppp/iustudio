40b7481a1c950fc6137186749ba83c3371325d6e
Cipppp <pirvuciprian37@gmail.com>
2024-05-15 21:37:51 +0300

    Updated IUstudio website part 4

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['iustudio-storage.s3.eu-north-1.amazonaws.com', 'placehold.co'],
+    },
+    // Your other Next.js configurations...
+  };
+  
+  export default nextConfig;
+  
