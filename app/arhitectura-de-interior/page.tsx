b2bfe696102d37054517f1e3b2dcc0b50593449a
Cipppp <pirvuciprian37@gmail.com>
2024-05-17 18:00:42 +0300

    Inversed cards

--- app/arhitectura-de-interior/page.tsx
+++ app/arhitectura-de-interior/page.tsx
@@ -6,12 +6,12 @@ export default function InteriorArhitecture() {
     return (
         <ProjectGallery
             folderNames={[
-                'Birou-BGA_an-2019',
-                'Birou-IU_an-2019_2020',
-                'Magazin-ROCA_an-2020',
                 'Casa-CC22', 
                 'Casa-NIU', 
-                'Casa-PRM22'
+                'Casa-PRM22',
+                'Birou-BGA_an-2019',
+                'Birou-IU_an-2019_2020',
+                'Magazin-ROCA_an-2020'
             ]}
             baseFolder="optimized/Magazin_incaltaminte"
             basePath="arhitectura-de-interior" // Pass a different basePath for interior architecture
